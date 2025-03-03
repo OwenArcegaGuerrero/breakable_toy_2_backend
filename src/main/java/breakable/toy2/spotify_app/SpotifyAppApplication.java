5f45586952ccfd6c2976d2f15ced83026dfcebbf
= <=>
2025-03-03 04:33:46 -0600

    added-refresh-token

--- src/main/java/breakable/toy2/spotify_app/SpotifyAppApplication.java
+++ src/main/java/breakable/toy2/spotify_app/SpotifyAppApplication.java
@@ -2,8 +2,10 @@ package breakable.toy2.spotify_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpotifyAppApplication {
 
 	public static void main(String[] args) {
